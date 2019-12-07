ab33c344cf3c9f3dc13e8154a1f9e86745ae2e1f
npodewitz <nils.podewitz@googlemail.com>
2019-12-07 20:47:42 +0100

    Refactor import script
    -> import script now uses a run and retry for key importing

--- scripts/etcd_import_dir.sh
+++ scripts/etcd_import_dir.sh
@@ -5,6 +5,30 @@ set -e
 
 # Variables
 source /usr/bin/environment.sh
+RETRY_SLEEP_TIME=1
+MAX_RETRY_ATTEMPTS=10
+
+# Functions
+function run_with_retry {
+    cmd=$1
+    max_retry=$2
+    sleep_time=$3
+    cmd_on_error=$4
+    cmd_on_success=$5
+
+    put_attempt=0
+    while [[ ${put_attempt} -lt ${max_retry} ]]; do
+	eval "${cmd}" && break || true
+	sleep $sleep_time
+	((put_attempt++)) || true
+    done
+    if [[ ${put_attempt} -ge ${max_retry} ]]; then
+	eval "${cmd_on_error}"
+    else
+	eval "${cmd_on_success}"
+    fi
+}
+
 
 # Set CMD_ETCDCTL as wrapper
 if [[ ! ${CMD_ETCDCTL+x} || ${CMD_ETCDCTL} == "" ]]; then
@@ -37,28 +61,40 @@ cd $1
 if [[ ${E3CH_MODE} -ne 0 ]]; then
     for i in $(find . -type d ! -name .); do
 	dir=${i:1}
-	logit "debug" "Creating directory key '${dir}'"
-	printf "%s" "${E3CH_DIR_VAL}" | ${CMD_ETCDCTL} put -- ${dir}
+	logit "debug" "Creating directory key ${dir@Q}"
+	run_with_retry \
+	    "printf \"%s\" \"${E3CH_DIR_VAL}\" | ${CMD_ETCDCTL} put -- ${dir}" \
+	    ${MAX_RETRY_ATTEMPTS} \
+	    ${RETRY_SLEEP_TIME} \
+	    "logit \"ERROR\" \"Creating directory key failed: ${dir@Q}\""
     done
 fi
 
 for i in $(find . -type f); do
     key=${i:1}
     if [[ "${key:0:1}" != "/" ]]; then
-	logit "info" "Skipping invalid key not starting with '/': '${key}'"
+	logit "info" "Skipping invalid key not starting with '/': ${key@Q}"
 	continue
     fi
     
-    logit "debug" "Importing key: '${key}'"
+    logit "debug" "Importing key: ${key@Q}"
 
     # Delete last character if it is a newline before putting into etcd
     # (makes editing much easier)
     if [[ "$(tail -c 1 $i | xxd -p -c 1 -l 1)" != "0a" ]]; then
 	logit "debug" "Importing without trimming"
-	cat $i | ${CMD_ETCDCTL} put -- ${key}
+	run_with_retry \
+	    "cat $i | ${CMD_ETCDCTL} put -- ${key}" \
+	    ${MAX_RETRY_ATTEMPTS} \
+	    ${RETRY_SLEEP_TIME} \
+	    "logit \"ERROR\" \"Creating directory key failed: ${dir@Q}\""
     else
 	logit "debug" "Importing with trimming"
-	cat $i | head -c -1 | ${CMD_ETCDCTL} put -- ${key}
+	run_with_retry \
+	    "cat $i | head -c -1 | ${CMD_ETCDCTL} put -- ${key}" \
+	    ${MAX_RETRY_ATTEMPTS} \
+	    ${RETRY_SLEEP_TIME} \
+	    "logit \"ERROR\" \"Creating directory key failed: ${dir@Q}\""
     fi
 done
 cd $TMP_CWD
